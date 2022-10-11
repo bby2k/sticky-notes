bfbc3f0aaf04250ab18c8d5780ccbff3a4252321
bby2k <bakbence.2k@gmail.com>
2022-10-11 09:28:28 +0200

    Refactor: Move App.css and App.js to components folder

---
+++ react/src/components/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from "./common/header";
+import Board from "./board";
+
+function App() {
+  return (
+    <div className="App">
+        <div>
+            <Header/>
+        </div>
+        <Board/>
+    </div>
+  );
+}
+
+export default App;
