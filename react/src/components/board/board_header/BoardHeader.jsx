5da4531aeade21f05439f6695f21d7a0135e85cd
bby2k <bakbence.2k@gmail.com>
2022-10-13 15:14:22 +0200

    Feat: Add conditional functionality to edit note button. If the user is granted editor permission, then it's shown.

--- react/src/components/board/board_header/BoardHeader.jsx
+++ react/src/components/board/board_header/BoardHeader.jsx
@@ -11,14 +11,16 @@ import {BoardContext} from "../../../context/BoardContext";
 function BoardHeader(){
     const {shownBoard} = useContext(BoardContext);
     return(
-        <div className={"container options-container"}>
-            <div className="row justify-content-center">
-                <button className={"options-btn"} type={"button"} id={"add-note"} data-bs-toggle={"modal"} data-bs-target={"#createNoteModal"} data-keybord={"false"}>
-                    <div className="col-2">
-                        <i className="bi bi-sticky options-icon"></i>
-                    </div>
-                </button>
-                <CreateNote/>
+            <div className={"container options-container"}>
+                <div className="row justify-content-center">
+                    {shownBoard.permission !== "EDITOR" ? <div className="col-2"></div> :
+                        <button className={"options-btn"} type={"button"} id={"add-note"} data-bs-toggle={"modal"} data-bs-target={"#createNoteModal"} data-keybord={"false"}>
+                            <div className="col-2">
+                                <i className="bi bi-sticky options-icon"></i>
+                            </div>
+                        </button>
+                    }
+                    <CreateNote/>
 
                 <div className="col-8 options-board-name">
                     Magic Board
