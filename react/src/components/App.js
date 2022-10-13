efcb6fe3a9501bb797e5b42ade99133d4ab5c00a
bby2k <bakbence.2k@gmail.com>
2022-10-13 15:08:27 +0200

    Feat: Add temporary board object for testing

--- react/src/components/App.js
+++ react/src/components/App.js
@@ -11,6 +11,22 @@ import {BoardContext} from "../context/BoardContext";
 
 
 function App() {
+    //TODO: This is a temporary test board, it will work dynamically with backend and axios fetching.
+    const welcomeBoard = {
+        name:"Welcome to Sticky Notes",
+        id:"board id : 1",
+        permission:"EDITOR",
+        notes:{
+            0:{
+                title:"Working on requests",
+                message:"Still working on request logic with Axios."},
+            1:{
+                title:"Better UX",
+                message:"The main point of this pet project is to be better with react."
+            }
+        }
+    }
+    let [shownBoard, setShownBoard] = React.useState(welcomeBoard);
   return (
     <div className="App">
         <div>
