d02dacc9673d1da93581b218ba12f8af6de6726d
bby2k <bakbence.2k@gmail.com>
2022-10-13 15:19:43 +0200

    Docs: Add index.js to notification folder for easier importing.

---
+++ react/src/components/common/index.js
@@ -0,0 +1,7 @@
+import Header from './header'
+import Loading from "./loading";
+import Menu from "./menu";
+import Navbar from "./navbar";
+import Notification from "./notification";
+
+export {Header, Loading, Menu, Navbar, Notification}
