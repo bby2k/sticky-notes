c38e2eeff0a424fa25b8dc95b54749936e14b0b9
bby2k <bakbence.2k@gmail.com>
2022-10-15 18:16:12 +0200

    Feat: Finalise Note components looks.

--- react/src/components/board/note/Note.jsx
+++ react/src/components/board/note/Note.jsx
@@ -4,9 +4,16 @@ import './Note.css'
 function Note(props){
     return(
         <div className={"note"}>
-            <h2>{props.title}</h2>
-            <p>{props.description}</p>
+            <div className="container note-content-container">
+                <h2>{props.title}</h2>
+                <p>{props.description}</p>
+            </div>
+            <div className="container note-fold-container">
+                <div className={"note-fold"} onClick={() => {moveToArchivedNotes()}}></div>
+            </div>
         </div>
     )
+}
+function moveToArchivedNotes(){
 }
 export default Note;
