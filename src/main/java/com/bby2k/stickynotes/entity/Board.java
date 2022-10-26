cb0378fa8f09b11fa008fff59c204bcf1026a933
bby2k <bakbence.2k@gmail.com>
2022-10-26 08:07:59 +0200

    Feat: Create Board entity with Java Persistence API.

---
+++ src/main/java/com/bby2k/stickynotes/entity/Board.java
@@ -0,0 +1,51 @@
+package com.bby2k.stickynotes.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Board {
+    @Id
+    private final UUID id = UUID.randomUUID();
+    private String title;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "joined_users",
+            joinColumns = @JoinColumn(name = "board_entity_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_entity_id")
+
+    )
+    @JsonIgnore
+    private Set<User> joinedUsers = new HashSet<>();
+
+    @OneToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "notes_in_board")
+    private Set<Note> notesInBoard = new HashSet<>();
+
+    public Board(String title) {
+        this.title = title;
+    }
+
+    public void joinTable(User user){
+        joinedUsers.add(user);
+    }
+
+    public void leaveTable(User user){
+        joinedUsers.remove(user);
+    }
+    public void addNote(Note note){
+        notesInBoard.add(note);
+    }
+    public void removeNote(Note note){
+        notesInBoard.remove(note);
+    }
+}
