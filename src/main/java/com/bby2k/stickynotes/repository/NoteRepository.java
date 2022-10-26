ed242236ec206b8f072c6680e72b92e918511b26
bby2k <bakbence.2k@gmail.com>
2022-10-26 08:14:56 +0200

    Feat: Using JpaRepository for Noteepository

--- src/main/java/com/bby2k/stickynotes/repository/NoteRepository.java
+++ src/main/java/com/bby2k/stickynotes/repository/NoteRepository.java
@@ -1,4 +1,11 @@
 package com.bby2k.stickynotes.repository;
 
-public interface NoteRepository {
+import com.bby2k.stickynotes.entity.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, UUID> {
 }
