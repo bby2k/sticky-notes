d1e6836cb5f56a981634aee8965328e8f1ca8510
bby2k <bakbence.2k@gmail.com>
2022-10-26 08:14:49 +0200

    Feat: Using JpaRepository for BoardRepository

--- src/main/java/com/bby2k/stickynotes/repository/BoardRepository.java
+++ src/main/java/com/bby2k/stickynotes/repository/BoardRepository.java
@@ -1,4 +1,12 @@
 package com.bby2k.stickynotes.repository;
 
-public class BoardRepository {
+import com.bby2k.stickynotes.entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, UUID> {
+    Board getBoardById(UUID id);
 }
