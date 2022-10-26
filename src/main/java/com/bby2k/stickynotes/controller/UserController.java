aab99723b6e007b57cfacb1fe250a52b6a8f3562
bby2k <bakbence.2k@gmail.com>
2022-10-26 08:09:53 +0200

    Feat: Implement basic CRUD methods.

--- src/main/java/com/bby2k/stickynotes/controller/UserController.java
+++ src/main/java/com/bby2k/stickynotes/controller/UserController.java
@@ -1,4 +1,35 @@
 package com.bby2k.stickynotes.controller;
 
+import com.bby2k.stickynotes.dto.UserDTO;
+import com.bby2k.stickynotes.entity.User;
+import com.bby2k.stickynotes.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping(value = "/user/{id}")
+    public UserDTO getUser(@PathVariable("id") UUID id) {
+        return userService.getUserDTOById(id);
+    }
+
+    @PostMapping(value = "/user", consumes = {"application/json"})
+    public void addUser(@RequestBody User user){
+        userService.addUser(user);
+    }
+
+    @PutMapping(value = "/user", consumes = {"application/json"})
+    public void updateUser(@RequestBody User user){
+        userService.updateUser(user);
+    }
+
+    @DeleteMapping(value = "/user", consumes = {"application/json"})
+    public void deleteUser(@RequestBody User user){
+        userService.removeUser(user);
+    }
 }
