6034d62ad8ea99845874318cc516caf4ecaa68d8
bby2k <bakbence.2k@gmail.com>
2022-10-26 08:16:07 +0200

    Feat: Basic WebSocket configurations for route "/boards"

--- src/main/java/com/bby2k/stickynotes/WebSocketConfig.java
+++ src/main/java/com/bby2k/stickynotes/WebSocketConfig.java
@@ -1,4 +1,23 @@
 package com.bby2k.stickynotes;
 
-public class WebSocketConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(final MessageBrokerRegistry registry){
+        registry.enableSimpleBroker("/boards");
+        registry.setApplicationDestinationPrefixes("/web-socket");
+
+    }
+    @Override
+    public void registerStompEndpoints(final StompEndpointRegistry registry){
+        registry.addEndpoint("/sticky-notes-websocket").withSockJS();
+    }
 }
