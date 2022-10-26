23e441851401c09ff4e124d953e40335367c6f42
bby2k <bakbence.2k@gmail.com>
2022-10-26 08:13:22 +0200

    Feat: Implement basic methods for NoteService

--- src/main/java/com/bby2k/stickynotes/service/NoteService.java
+++ src/main/java/com/bby2k/stickynotes/service/NoteService.java
@@ -1,4 +1,22 @@
 package com.bby2k.stickynotes.service;
 
+import com.bby2k.stickynotes.entity.Note;
+import com.bby2k.stickynotes.repository.NoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
 public class NoteService {
+    @Autowired
+    private NoteRepository noteRepository;
+
+    public void addNote(Note note, UUID boardId){
+        noteRepository.save(note);
+    }
+
+    public void saveNote(Note note) {
+        noteRepository.save(note);
+    }
 }
