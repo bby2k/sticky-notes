dfbe7fe4934277792580e4ca85b5333763cf34fd
bby2k <bakbence.2k@gmail.com>
2022-10-26 08:15:14 +0200

    Feat: Using JpaRepository for UserRepository

--- src/main/java/com/bby2k/stickynotes/repository/UserRepository.java
+++ src/main/java/com/bby2k/stickynotes/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package com.bby2k.stickynotes.repository;
 
-public interface UserRepository {
+import com.bby2k.stickynotes.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    User getUserById(UUID uuid);
 }
