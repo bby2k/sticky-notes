dfd34c3281e575e9c58b4982a849f89acf4504f3
bby2k <bakbence.2k@gmail.com>
2023-02-13 09:12:53 +0100

    feat: Extend UserRepository, change return values with Optional

--- src/main/java/com/bby2k/stickynotes/repository/UserRepository.java
+++ src/main/java/com/bby2k/stickynotes/repository/UserRepository.java
@@ -2,12 +2,26 @@ package com.bby2k.stickynotes.repository;
 
 import com.bby2k.stickynotes.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+    User save(User user);
 
-    User getUserById(UUID uuid);
+    UserDetails findByUsername(String username);
+
+    Optional<User> getUserById(UUID uuid);
+    Optional<User> getUserByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    void delete(User user);
+
+    void deleteByUsername(String username);
+
+    void deleteByEmail(String email);
 }
